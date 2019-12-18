79ccfef726bb341dd61bd9e74abefdbfa9b559e3
Rainer Jacob <r.jacob@live.de>
2019-12-18 13:44:03 +0100

    distance functionality

--- epc_lib/epc_math.py
+++ epc_lib/epc_math.py
@@ -155,6 +155,6 @@ def distance_correction(dist, error_polynom):
     dist = dist.astype('float32')
     error = np.polyval(error_polynom, dist)
     # neglect distances below 300
-    # dist = np.where(dist < 300, dist, dist-error)
+    dist = np.where(dist < 300, dist, dist-error)
 
     return dist
